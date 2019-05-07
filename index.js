79aff45326df5e27d48c8148ba309dc5e47e1a25
Lucas Klefasz Ferreira <ferreirak.lucas@gmail.com>
2019-05-07 23:06:52 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,11 +10,38 @@ function setCart(c) {
 }
 
 function addToCart(item) {
- // write your code here
+    var itemObj = Object.assign({}, {itemName: item}, {itemPrice: Math.floor(Math.random() * 100) + 1})
+    cart.push(itemObj)
+    return `${item} has been added to your cart.`
 }
 
 function viewCart() {
-  // write your code here
+  var itemNamesAndPrices = 'In your cart, you have '
+
+  if(cart.length == 0) {
+    return 'Your shopping cart is empty.'
+  } else {
+
+  for (var i = 0; i < cart.length; i++) {
+
+      if (cart.length == 1) {
+      return `In your cart, you have ${cart[0]['itemName']} at $${cart[0]['itemPrice']}.`
+
+    } else if (cart.length == 2) {
+      return `In your cart, you have ${cart[0]['itemName']} at $${cart[0]['itemPrice']}, and ${cart[1]['itemName']} at $${cart[1]['itemPrice']}.`
+
+    } else {
+      if (i < (cart.length - 1)) {
+      itemNamesAndPrices = itemNamesAndPrices + `${cart[i]['itemName']} at $${cart[i]['itemPrice']}, `
+    } else {
+      itemNamesAndPrices = itemNamesAndPrices + `and ${cart[i]['itemName']} at $${cart[i]['itemPrice']}.`
+    }
+
+    }
+
+  }
+  return itemNamesAndPrices
+}
 }
 
 function total() {

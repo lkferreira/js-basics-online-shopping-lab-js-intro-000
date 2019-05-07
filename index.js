c4ca3c60ae0556bf04ab234bf7ead25793c29211
Lucas Klefasz Ferreira <ferreirak.lucas@gmail.com>
2019-05-07 23:26:39 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -53,7 +53,15 @@ function total() {
 }
 
 function removeFromCart(item) {
-  // write your code here
+  for (var i = 0; i < cart.length; i++) {
+    if (cart[0][itemName] == item) {
+      cart.splice(i,1)
+    }
+    else {
+      return "That item is not in your cart."
+    }
+  }
+  return cart
 }
 
 function placeOrder(cardNumber) {
